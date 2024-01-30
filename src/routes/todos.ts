d891a1081c2b9e6523c2de54aa3f07ea876c897d
gitTanzj <kalle.riit@gmail.com>
2024-01-30 10:41:03 +0200

    Update the Todo by id

--- src/routes/todos.ts
+++ src/routes/todos.ts
@@ -1,11 +1,11 @@
 import { Router } from 'express';
-import { createTodo, getTodos } from '../controllers/todos'
+import { createTodo, getTodos, updateTodo } from '../controllers/todos'
 
 const router = Router()
 
 router.post('/', createTodo)
-router.get('/')
-router.patch('/:id')
+router.get('/', getTodos)
+router.patch('/:id', updateTodo)
 router.delete('/:id')
 
 export default Router
