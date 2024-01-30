3c021e3fb1496d0fe800b04edec42392409184d4
gitTanzj <kalle.riit@gmail.com>
2024-01-30 10:38:20 +0200

    Create new Todo from user data

--- dist/app.js
+++ dist/app.js
@@ -5,8 +5,13 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 Object.defineProperty(exports, "__esModule", { value: true });
 const express_1 = __importDefault(require("express"));
 const todos_1 = __importDefault(require("./routes/todos"));
+const body_parser_1 = require("body-parser");
 const app = (0, express_1.default)();
+app.use((0, body_parser_1.json)());
 app.use('/todos', todos_1.default);
+app.use((error, req, res, next) => {
+    res.json({ message: error.message });
+});
 app.listen(3011, () => {
-    console.log('Server is started at https://localhost:3011');
+    console.log('Server is started at http://localhost:3011');
 });
